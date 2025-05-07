e95db514147eac9a1e72df9fbe0c784174f25e3c
woliver99 <oliver@maplenetwork.ca>
2025-05-07 15:24:27 -0400

    Ready for Public Release

---
+++ README.md
@@ -0,0 +1,8 @@
+# Open Automate
+
+-   A personal automation app for Android
+-   Utilizes Shizuku to function without root access
+
+## Features
+
+-   Papa Mode: Disables all wireless connectivity on the device
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,9 +17,19 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+            storeFile = file(System.getenv("KEYSTORE_PATH") ?: "./keystore.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+        }
+    }
+
     buildTypes {
         release {
-            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
